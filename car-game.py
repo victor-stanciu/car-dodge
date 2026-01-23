5c623ba71fbad9df43f9140e13ee9e9ae010f4d3
victor-stanciu <victorstanciu.st@gmail.com>
2026-01-23 16:25:02 +0200

    - Add message_display & text_objects functions for displaying custom messages (death-screen)

--- car-game.py
+++ car-game.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import random
 
 pygame.init()
 
@@ -10,6 +12,7 @@ pygame.display.set_caption('A bit Racey')
 
 black = (0,0,0)
 white = (255, 255, 255)
+red = (255, 0, 0)
 
 car_width = 73
 
@@ -18,23 +21,44 @@ crashed = False
 carImg = pygame.image.load('racecar.png')
 carImg = pygame.transform.scale(carImg, (100, 200))
 
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
+
+def text_objects(text, font, color):
+    textSurface = font.render(text, True, red)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 100)
+    TextSurf, TextRect = text_objects(text, largeText, red)
+    TextRect.center = ((display_width/2), (display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+def crash():
+    message_display("You Crashed")
 
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.750)
     x_change = 0
     car_speed = 0
+    thing_starx = random.randrange(0, display_width)
 
     gameExit = False
 
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
-
+                pygame.quit()
+                quit
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
@@ -45,12 +69,13 @@ def game_loop():
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
 
-            print(x)
+
 
             # How to just set a boundary though? 
             
             if x > display_width - car_width or x < 0:
                 print('CRASH')
+                crash()
                 gameExit = True
 
         x += x_change 
